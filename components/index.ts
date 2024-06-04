75dc5b273c48b63a7e7e0dd6ea7c2ba7079cc3ac
brianrojas <briaan.dc@gmail.com>
2024-06-04 03:54:53 -0300

    estructura de componentes

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as About } from "./about/About";
+export { default as Servicios } from "./servicios/Servicios";
+export { default as Contact } from "./contact/Contact";
+
+// client components apartir de aqui
