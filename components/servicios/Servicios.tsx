563802ed7b1d24dd0e1e775e695fffd24139719e
brianrojas <briaan.dc@gmail.com>
2024-06-05 19:54:52 -0300

    esqueleto de servicios

--- components/servicios/Servicios.tsx
+++ components/servicios/Servicios.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Servicios = () => {
-  return <div>Servicios</div>;
+  return (
+    <div>
+      <div>
+        <h2>Servicios</h2>
+      </div>
+      <div>
+        <h4>Mezcla</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, consequuntur aperiam porro officia dicta
+          temporibus quasi tenetur explicabo a error labore asperiores delectus unde molestias reprehenderit optio omnis
+          ullam! Tempore?
+        </p>
+      </div>
+      <div>
+        <h4>Master</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis sit earum aperiam modi, dolorem quaerat
+          quae ipsum assumenda accusantium voluptates non voluptatum voluptatibus culpa! Fuga quia odit exercitationem
+          id. Rem!
+        </p>
+      </div>
+      <div>
+        <h4>Post-Produccion</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam voluptatibus eveniet laboriosam, dolorum
+          culpa natus impedit! Veritatis hic, nesciunt, neque nulla corrupti accusamus, rerum explicabo blanditiis dolor
+          laboriosam voluptate quos.
+        </p>
+      </div>
+      <div>
+        <h4>Grabacion de voces</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias earum molestiae culpa eum illo atque
+          vel beatae cumque blanditiis? Quaerat reiciendis esse ducimus ipsam ea consequatur perferendis quam
+          consectetur!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Servicios;
