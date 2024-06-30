4d773fdc5f740680ce2dfd42367daf958c1f94e2
brianrojas <briaan.dc@gmail.com>
2024-06-30 07:39:14 -0300

    avance de services

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import { Servicios, Contact } from "@/components";
 import { Roboto_Condensed, Cabin } from "next/font/google";
+import Mixer from "@/components/servicios/Mixer";
+import Music from "@/components/servicios/Music";
+import Mic from "@/components/servicios/Mic";
+import Soundwave from "@/components/servicios/Soundwave";
 
 const roboto = Roboto_Condensed({ subsets: ["latin"] });
 const cabin = Cabin({ subsets: ["latin"] });
@@ -10,14 +13,14 @@ export default function Home() {
   return (
     <main className="mx-auto items-center">
       <section
-        className="relative bg-[url('/Consola.jpg')] bg-center bg-cover object-cover opacity-1 h-full lg:min-h-screen flex lg:justify-end lg:px-36 m-0"
+        className="relative  bg-[url('/Consola.jpg')] fill-current bg-center bg-cover object-cover  h-auto flex lg:justify-end lg:px-36 m-0"
         id="home"
       >
-        <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent lg:bg-gradient-to-r lg:from-transparent lg:to-black"></div>
-        {/* Overlay oscuro */}
+        <div className="lg:absolute lg:inset-0  lg:bg-gradient-to-r lg:from-transparent lg:to-black"></div>
+       
 
         <div className="relative w-full lg:w-1/2 flex flex-col items-start text-center justify-center p-8 z-10">
-          {/* Mitad derecha con el texto */}
+          
           <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-white">Where Sound Comes To Life</h1>
           <h2 className="text-2xl lg:text-4xl text-white">
             Let your music echo through the ages at Echo Chamber Studios
@@ -26,12 +29,12 @@ export default function Home() {
       </section>
 
       <section className="bg-[#1B262C] flex py-10 px-2 lg:py-24">
-        <div className="flex flex-col-reverse justify-center lg:grid lg:grid-cols-2 lg:gap-2 lg:px-24 ">
-          <div className=" text-center py-4">
+        <div className="flex flex-col-reverse justify-center lg:grid lg:grid-cols-2 lg:gap-6 lg:px-24 ">
+          <div className="flex flex-col py-4 lg:text-md text-sm">
             <h1 className="text-2xl lg:text-4xl font-extrabold text-white">ABOUT US</h1>
-            <hr className="border-t-4 border-[#C06048] my-6 w-28 mx-auto lg:mx-0" />
+            <span className="border-t-4 border-[#C06048] my-6 w-28 lg:mx-auto" />
 
-            <p className="text-md">Welcome to Echo Chamber Studios, where music and creativity flow freely.</p>
+            <p>Welcome to Echo Chamber Studios, where music and creativity flow freely.</p>
             <br />
             <p>
               Our state-of-the-art studio is designed to bring your musical vision to life, whether a seasoned
@@ -59,11 +62,52 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section>
-        <Servicios />
+      <section className="m-auto p-4 pt-0">
+        <div className="flex justify-center pb-6">
+          <h2 className="text-2xl">Servicios</h2>
+        </div>
+        <div className="flex justify-center ">
+          <span className="px-1 py-1 bg-[#c0604863] rounded-full"></span>
+          <span className="px-6 py-1 bg-[#c06048] rounded-full mx-2" />
+          <span className="px-1 py-1 bg-[#c0604863] rounded-full"></span>
+        </div>
+        <div className="lg:grid lg:grid-cols-4 lg:gap-4 lg:justify-center text-center gap-4 py-4 ">
+          <div className="p-6 flex flex-col items-center justify-center">
+            <h4 className="py-2">Mezcla</h4>
+            <Mixer />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, consequuntur aperiam porro officia
+              dicta
+            </p>
+          </div>
+          <div className="p-6 flex flex-col items-center justify-center">
+            <h4 className="py-2">Master</h4>
+            <Music />
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis sit earum aperiam modi, dolorem
+              quaerat
+            </p>
+          </div>
+          <div className="p-6 flex flex-col items-center justify-center">
+            <h4 className="py-2">Post-Produccion</h4>
+            <Soundwave />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam voluptatibus eveniet laboriosam,
+              dolorum
+            </p>
+          </div>
+          <div className="p-6 flex flex-col items-center justify-center">
+            <h4 className="py-2">Grabacion de voces</h4>
+            <Mic />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo molestias earum molestiae culpa eum illo
+            </p>
+          </div>
+        </div>
       </section>
       <section>
-        <Contact />
+        <h4>Contact</h4>
       </section>
     </main>
   );
--- components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Servicios } from "./servicios/Servicios";
-export { default as Contact } from "./contact/Contact";
-
-// client components a partir de aquí
