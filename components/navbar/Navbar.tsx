ed8ca34ea831e801224cd42bf7a350a34d99d583
brianrojas <briaan.dc@gmail.com>
2024-07-03 20:36:27 -0300

    maquetacion de nabvar terminado

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,18 +1,23 @@
-import { ActiveLink } from "../active-link/ActiveLink";
-
-const navItems = [
-  { path: "#about", text: "About" },
-  { path: "#home", text: "Home" },
-  { path: "#contact", text: "Contact" },
-  { path: "#servicios", text: "Servicios" },
-];
+import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      {navItems.map((navItem) => (
-        <ActiveLink key={navItem.path} {...navItem} />
-      ))}
+    <nav className="flex justify-center bg-[#C06048] bg-opacity-30 p-2 rounded">
+      <div className="flex items-center">
+        <ul className="flex px-2">
+          <li className="px-2">Inicio</li>
+          <li className="px-2">Sobre Nosotros</li>
+        </ul>
+      </div>
+      <div>
+        <Image src="/next_sound.png" alt="logo" width={40} height={40} />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex px-2">
+          <li className="px-2">Servicios</li>
+          <li className="px-2">Contacto</li>
+        </ul>
+      </div>
     </nav>
   );
 };
