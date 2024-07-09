009e690b78a10836d7d7628568b3871c308670c4
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-07-09 02:46:15 -0300

    prueba 6

--- app/utils/prisma.utils.ts
+++ app/utils/prisma.utils.ts
@@ -1,19 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-// Función para crear una nueva instancia de PrismaClient
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-// Declarar globalThis con prismaGlobal para TypeScript
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal?: PrismaClient;
 } & typeof global;
 
-// Crear la instancia de PrismaClient
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-// Asignar la instancia a globalThis.prismaGlobal solo en desarrollo
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
-
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
