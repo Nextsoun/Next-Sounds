60e22cc893c2ef90313f3386e6d3b4b915303b60
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-07-09 01:11:58 -0300

    se le agrega el try catch

--- app/actions/contact.actions.ts
+++ app/actions/contact.actions.ts
@@ -4,41 +4,45 @@ import { compileAdminTemplate, compileContactTemplate, sendAdminEmail, sendConta
 import prisma from "../utils/prisma.utils";
 
 export const createContact = async (formData: FormData) => {
-  const contactName = formData.get("name")?.toString();
-  const contactPhone = formData.get("phone")?.toString();
-  const contactEmail = formData.get("email")?.toString();
-  const contactMessage = formData.get("message")?.toString();
-  console.log({ formData });
+  try {
+    const contactName = formData.get("name")?.toString();
+    const contactPhone = formData.get("phone")?.toString();
+    const contactEmail = formData.get("email")?.toString();
+    const contactMessage = formData.get("message")?.toString();
+    console.log({ formData });
 
-  if (!contactName || !contactPhone || !contactEmail || !contactMessage) return;
+    if (!contactName || !contactPhone || !contactEmail || !contactMessage) return;
 
-  const newContact = await prisma.contact.create({
-    data: {
+    const newContact = await prisma.contact.create({
+      data: {
+        name: contactName,
+        phone: contactPhone,
+        email: contactEmail,
+        message: contactMessage,
+      },
+    });
+    console.log({ newContact });
+
+    const body = compileAdminTemplate(contactName, contactMessage, contactPhone, contactEmail);
+
+    await sendAdminEmail({
       name: contactName,
-      phone: contactPhone,
-      email: contactEmail,
       message: contactMessage,
-    },
-  });
-  console.log({ newContact });
+      email: contactEmail,
+      phone: contactPhone,
+      body: body,
+    });
 
-  const body = compileAdminTemplate(contactName, contactMessage, contactPhone, contactEmail);
+    const bodyContact = compileContactTemplate(contactName);
 
-  await sendAdminEmail({
-    name: contactName,
-    message: contactMessage,
-    email: contactEmail,
-    phone: contactPhone,
-    body: body,
-  });
+    await sendContactEmail({
+      name: contactName,
+      email: contactEmail,
+      bodyContact: bodyContact,
+    });
 
-  const bodyContact = compileContactTemplate(contactName);
-
-  await sendContactEmail({
-    name: contactName,
-    email: contactEmail,
-    bodyContact: bodyContact,
-  });
-
-  return newContact;
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
 };
