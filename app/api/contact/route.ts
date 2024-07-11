7a1514e66985325922964064386496ca0802bc58
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-07-11 02:50:51 -0300

    test 13

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,21 +1,11 @@
 import prisma from "@/app/utils/prisma.utils";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      const data = req.body;
-      const newContact = await prisma.contact.create({
-        data,
-      });
-      console.log({ newContact });
-
-      return res.status(200).json({ message: "Registro guardado correctamente." });
-    } catch (error) {
-      console.error("Error al guardar el registro:", error);
-      return res.status(500).json({ error: "Ocurrió un error al guardar el registro." });
-    }
-  } else {
-    return res.status(405).json({ error: "Método no permitido" });
-  }
+export async function POST(request: Request) {
+  const data = await request.json();
+  const newContact = await prisma.contact.create({
+    data,
+  });
+  console.log({ newContact });
+  return NextResponse.json("guardando registro ...");
 }
