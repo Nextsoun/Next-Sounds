063936cf7115a5bf9c4e81dd0fefb6e20c1bf196
VijarraM <121908120+VijarraM@users.noreply.github.com>
2024-07-12 00:48:08 -0300

    se aplica mongo db base datos

---
+++ app/actions/contact.actions.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { compileAdminTemplate, compileContactTemplate, sendAdminEmail, sendContactEmail } from "../utils/mail.utils";
+import prisma from "../lib/prisma";
+
+export const createContact = async (formData: FormData) => {
+  try {
+    const contactName = formData.get("name")?.toString();
+    const contactPhone = formData.get("phone")?.toString();
+    const contactEmail = formData.get("email")?.toString();
+    const contactMessage = formData.get("message")?.toString();
+
+    if (!contactName || !contactPhone || !contactEmail || !contactMessage) return;
+
+    const newContact = await prisma.contact.create({
+      data: {
+        name: contactName,
+        phone: contactPhone,
+        email: contactEmail,
+        message: contactMessage,
+      },
+    });
+
+    const body = compileAdminTemplate(contactName, contactMessage, contactPhone, contactEmail);
+
+    await sendAdminEmail({
+      name: contactName,
+      message: contactMessage,
+      email: contactEmail,
+      phone: contactPhone,
+      body: body,
+    });
+
+    const bodyContact = compileContactTemplate(contactName);
+
+    await sendContactEmail({
+      name: contactName,
+      email: contactEmail,
+      bodyContact: bodyContact,
+    });
+
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,78 +1,75 @@
 "use client";
+import { createContact } from "@/app/actions/contact.actions";
 import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 const Contact = () => {
-  const { handleSubmit, register } = useForm();
-  // const formRef = useRef<HTMLFormElement>(null);
+  const formRef = useRef<HTMLFormElement>(null);
 
-  // const validateForm = (formData: FormData) => {
-  //   const name = formData.get("name")?.toString().trim();
-  //   const phone = formData.get("phone")?.toString().trim();
-  //   const email = formData.get("email")?.toString().trim();
-  //   const message = formData.get("message")?.toString().trim();
+  const validateForm = (formData: FormData) => {
+    const name = formData.get("name")?.toString().trim();
+    const phone = formData.get("phone")?.toString().trim();
+    const email = formData.get("email")?.toString().trim();
+    const message = formData.get("message")?.toString().trim();
 
-  //   if (!name || !phone || !email || !message) {
-  //     toast.warning("Todos los campos son obligatorios.");
-  //     return false;
-  //   }
+    if (!name || !phone || !email || !message) {
+      toast.warning("Todos los campos son obligatorios.");
+      return false;
+    }
 
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   if (!emailRegex.test(email)) {
-  //     toast.warning("Por favor, ingrese un email válido.");
-  //     return false;
-  //   }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      toast.warning("Por favor, ingrese un email válido.");
+      return false;
+    }
 
-  //   return true;
-  // };
+    return true;
+  };
 
-  // const handleSubmit = async (formData: FormData) => {
-  //   if (!validateForm(formData)) {
-  //     return;
-  //   }
-  const onSubmit = handleSubmit(async (data) => {
-    const res = await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const resJSON = await res.json;
-    console.log(resJSON);
-  });
+  const handleSubmit = async (formData: FormData) => {
+    if (!validateForm(formData)) {
+      return;
+    }
 
-  // try {
-  // const contactPromise = createContact(formData);
-  // toast.promise(contactPromise, {
-  //   loading: "Enviando...",
-  //   success: (data) => {
-  //     console.log(data);
+    try {
+      const contactPromise = createContact(formData);
+      toast.promise(contactPromise, {
+        loading: "Enviando...",
+        success: (data) => {
+          if (data) {
+            return `${data.name} tu solicitud fue enviada.`;
+          } else {
+            return "Solicitud enviada.";
+          }
+        },
+        error: (err) => `Error: ${err.message}`,
+      });
 
-  //     return ` tu solicitud fue enviada.`;
-  //   },
-  //   error: (err) => `Error: ${err.message}`,
-  // });
+      // const contact = await contactPromise;
 
-  // const contact = await contactPromise;
-
-  // if (formRef.current) {
-  //   formRef.current.reset();
-  // }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+      if (formRef.current) {
+        formRef.current.reset();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <form onSubmit={onSubmit} noValidate>
+    <form
+      ref={formRef}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (formRef.current) {
+          const formData = new FormData(formRef.current);
+          handleSubmit(formData);
+        }
+      }}
+      noValidate
+    >
       <div className="mb-4">
         <input
           type="text"
-          {...register("name", {
-            required: true,
-          })}
           name="name"
           id="name"
           placeholder="Nombre"
@@ -82,9 +79,6 @@ const Contact = () => {
       <div className="mb-4">
         <input
           type="text"
-          {...register("phone", {
-            required: true,
-          })}
           name="phone"
           id="phone"
           placeholder="Teléfono"
@@ -94,9 +88,6 @@ const Contact = () => {
       <div className="mb-4">
         <input
           type="email"
-          {...register("email", {
-            required: true,
-          })}
           name="email"
           id="email"
           placeholder="Dirección de Email"
@@ -105,9 +96,6 @@ const Contact = () => {
       </div>
       <div className="mb-4">
         <textarea
-          {...register("message", {
-            required: true,
-          })}
           name="message"
           id="message"
           placeholder="Mensaje"
